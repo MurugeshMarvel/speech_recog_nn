b69930ecd94ad36d979608170efaffa3c41200d5
MurugesMarvel <murugeshmarvel95@zoho.com>
2017-02-15 11:32:24 +0530

    updated the frames segmentation

--- codes/lpc.py
+++ codes/lpc.py
@@ -15,7 +15,6 @@ while True:
 	iteration+=1
 
 
-print value
 mean = np.mean(value) 	
 print "The mean value is"
 print mean
@@ -26,17 +25,17 @@ cons_value = value - mean
 print ("the noise free value is %",cons_value)
 min = np.min(value)
 print min
-print "The 2nd value"
-print value[2:5]
  
 print "Saved the feature vector fot RNN"
 
 print "constructing frames"
 
 no_of_frames = iteration/20
+print "the iteration is", iteration
 
 if ((iteration%20)!=0):
 	frame_residue = iteration%20
+	no_of_frames = no_of_frames+1
 
 print type(value[2])
 frame_length = 20
@@ -45,13 +44,22 @@ frame_value = np.empty((no_of_frames,frame_length))
 frame_node = 0
 for iter1 in range(0,no_of_frames):
 	for iter2 in range(0,frame_length):
-
-		frame_value[iter1][iter2] = value[frame_node]
+		
+		if ((frame_node-(iteration-1)<=0)):
+			frame_value[iter1][iter2] = value[frame_node]
+			#print frame_node
+		else:
+			frame_value[iter1][iter2] = 0
 		iter2+=1
 		frame_node+=1
 	iter1+=1
+
+print "The Non-framed value is " 
+print value
+
+print "The Framed Value is"
 print frame_value
 
 print np.shape(frame_value)
 
-print "The number of frames = %",no_of_frames
+print "The number of frames = ",no_of_frames
