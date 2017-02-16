73b4436ab93f93b6352c75621513b42dce8ea1be
MurugesMarvel <murugeshmarvel95@zoho.com>
2017-02-16 16:21:24 +0530

    Working in LPC encoding

--- codes/lpc.py
+++ codes/lpc.py
@@ -1,68 +1,23 @@
-import speech_input as si
 import numpy as np
-#initialise the input parameter by calling the speech_input parameters
-inp =   si.input()
-inp.get_sound()
 
-value = np.empty([0,0])
-limit = input("Enter the time limit to record")
-iteration = 0
-while True:
-	if (iteration<limit):
-		value=np.append(value,int(inp.get_sound()))
-	else:
-		break
-	iteration+=1
+class lpc(object):
+	def encode(input_sig,order,window):
+		signal = stackOLA(input_sig,window)
+		new_window, n = size(signal)
+		##lpc encode
+		coeff = np.zeros((order, n))
+		sig_power = np.zeros((1,n))
+		error_sig = np.zeros((new_window,n))
+		for i in range(0:n):
+			new_coeff, new_sig_power, new_error_sig = myplc(signal[i],order)
+			coeff[i] = new_coeff
+			sig_power[i] = new_sig_power
+			error_sig[2:new_window] = new_error_sig[i]
+		return coeff, sig_power,error_sig
 
 
-mean = np.mean(value) 	
-print "The mean value is"
-print mean
-print "The standard Deviation is "
-stand = np.std(value)
-print stand
-cons_value = value - mean
-print ("the noise free value is %",cons_value)
-min = np.min(value)
-print min
- 
-print "Saved the feature vector fot RNN"
 
-print "constructing frames"
 
-no_of_frames = iteration/20
-print "the iteration is", iteration
-
-if ((iteration%20)!=0):
-	frame_residue = iteration%20
-	no_of_frames = no_of_frames+1
-
-print type(value[2])
-frame_length = 20
-frame_value = np.empty((no_of_frames,frame_length))
-
-frame_node = 0
-for iter1 in range(0,no_of_frames):
-	for iter2 in range(0,frame_length):
-		
-		if ((frame_node-(iteration-1)<=0)):
-			frame_value[iter1][iter2] = value[frame_node]
-			#print frame_node
-		else:
-			frame_value[iter1][iter2] = 0
-		iter2+=1
-		frame_node+=1
-	iter1+=1
-
-print "The Non-framed value is " 
-print value
-
-print "The Framed Value is"
-print frame_value
-
-print np.shape(frame_value)
-
-print "The number of frames = ",no_of_frames
-value = str(value)
-frame_value = str(frame_value)
-inp.save_sound(filename="lpc1",value=value,frame=frame_value)
+if __name__ == '__main__':
+	lpc_object = lpc()
+	lpc.encode()
--- codes/lpc_main.py
+++ codes/lpc_main.py
@@ -1,23 +1,41 @@
 import soundfile as sf
 import scipy.signal as scisig
 import numpy as np
-
+import matplotlib.pyplot as plt
 data, fs = sf.read("speech2.wav")
 
 data_len =  len(data)
-#data_resam = scisig.resample(data,8000)
+
 
 max_data= max(abs(data))
 #print max_data
 data_norm = np.array([])
 
-for i in range(0,(data_len-1)):
-	data_norm = np.append(data_norm,0.9*data[i]/max_data)
+for j in range(0,data_len):
+	data_norm = np.append(data_norm,0.9*data[j]/max_data)
+
+print j
 
 
-print data_norm[0]
-print data_norm[1]
-print data_norm[2]
+data_resam = scisig.resample(data_norm,8000)
 
-print fs
-print data[50000]
+data_resam_len = len(data_resam)
+print "The first five normalised data is"
+for i in range(0,5):
+	print data_norm[i]
+
+print "The first five resamples data are"
+
+for i in range(0,5):
+	print data_resam[i]
+
+
+'''plt.plot(data_len,data_norm,'go-',data_resam_len,data_resam,'.-')
+pl.legend(['data','resampled'],loc='best')
+plt.show()'''
+print len(data_resam)
+
+print "the shape of the data_norm"
+print np.shape(data_norm)
+print "the shape of the resamples data"
+print np.shape(data_resam)
